abdbe8538fd49290aa6b031df0b9f4ecf7537611
Juan <salazarj@mit.edu>
2019-06-17 23:29:16 -0400

    Add files via upload

---
+++ ISS_Tracker_Jared.py
@@ -0,0 +1,31 @@
+# Juan Salazar 5/31/2018
+# Uses PyEphem Package to compute the position of the ISS 
+# and to write it in terms of altitude (degrees above horizon, +N) and azimuth (degrees along horizon, +E) 
+# Altitude & Azimuth are measured from a particular location at a particular time (the observer)
+
+import math
+import time
+from datetime import datetime
+import ephem
+home = ephem.Observer()
+# Long. and Lat. of observer
+home.lon, home.lat = '-71.19', '42.42'
+# Boston Long and Lat: '-71.19', '42.42'
+# Miami Long and Lat: '-80.1918' , '25.7617'   
+# Approximate elevation of observer in meters
+home.elevation = 6
+# NORAD TLE (Two Line Element) taken from https://www.celestrak.com/NORAD/elements/stations.txt, website updates several times daily
+# TLE's were invented to package all of the important information about the Keplerian orbits of artificial satellites, 
+# initially designed for 80-hole punch cards
+iss = ephem.readtle("ISS (ZARYA)", 
+	"1 25544U 98067A   19168.89165794  .00000725  00000-0  19994-4 0  9994",
+	"2 25544  51.6438 357.8011 0008176  60.1415  17.8944 15.51206633175360"
+)     
+   
+while True:
+	home.date = datetime.now()
+	iss.compute(home)
+	print('Altitude: %f - Azimuth: %f' % (math.degrees(iss.alt), math.degrees(iss.az)))
+# print("%s %s" % (ephem.degrees(iss.sublong), ephem.degrees(iss.sublat)))
+	print(home.date)
+	time.sleep(1) 
