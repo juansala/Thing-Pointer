ee09fa27f0e6f2dc17552a9857335cb23fc6095e
Juan <salazarj@mit.edu>
2019-06-23 21:54:29 -0400

    Update orbit_tracker.ino

--- orbit_tracker.ino
+++ orbit_tracker.ino
@@ -1,14 +1,16 @@
 #include <Servo.h>
-#define coil_A_1_pin  4
-#define coil_A_2_pin  17
-#define coil_B_1_pin  23
-#define coil_B_2_pin  24
+#define coil_A_1_pin  23
+#define coil_A_2_pin  22
+#define coil_B_1_pin  21
+#define coil_B_2_pin  20
 
 
 Servo arrow;
 int servo_pin = 9;
 int enable_pin = 18;
 int stepDelay = 10;
+int sprockRatio = 0;
+float deg2step = 0;
 
 // Ephem variables
 float az; // reported azimuth
@@ -16,20 +18,26 @@ float alt; //reported altitude
 
 void setup() {
   Serial.begin(9600);
-  digitalWrite(enable_pin, HIGH);
   arrow.attach(servo_pin);  
-
+  pinMode(23, OUTPUT);
+  pinMode(22, OUTPUT);
+  pinMode(21, OUTPUT);
+  pinMode(20, OUTPUT);
+  forward(50, 20);
+  backwards(50, 20);
+  
 }
 
 void loop() {
-  while (Serial.available())  {
-    alt = Serial.parseFloat();
-    az = Serial.parseFloat();
-    Serial.flush();
-    Serial.print(alt);
-    Serial.print(' ');
-    Serial.println(az);
-    } 
+//  while (Serial.available())  {
+//    alt = Serial.parseFloat();
+//    az = Serial.parseFloat();
+//    Serial.flush();
+//    Serial.print(alt);
+//    Serial.print(' ');
+//    Serial.println(az);
+//    } 
+  
 }
 
 
@@ -46,26 +54,26 @@ void setStep(boolean first, boolean sec, boolean third, boolean fourth){
 
 void forward(int dt, int steps){
   for (int i  = 0; i <= steps; i++){
-    setStep(1, 0, 1, 0);
+    setStep(1, 0, 0, 0);
     delay(dt);
-    setStep(0, 1, 1, 0);
+    setStep(0, 1, 0, 0);
     delay(dt);
-    setStep(0, 1, 0, 1);
+    setStep(0, 0, 1, 0);
     delay(dt);
-    setStep(1, 0, 0, 1);
+    setStep(0, 0, 0, 1);
     delay(dt);
   }
 }
 
 void backwards(int dt, int steps){
   for (int i = 0; i <= steps; i++){
-    setStep(1, 0, 0, 1);
+    setStep(0, 0, 0, 1);
     delay(dt);
-    setStep(0, 1, 0, 1);
+    setStep(0, 0, 1, 0);
     delay(dt);
-    setStep(0, 1, 1, 0);
+    setStep(0, 1, 0, 0);
     delay(dt);
-    setStep(1, 0, 1, 0);
-    delay(dt); 
+    setStep(1, 0, 0, 0);
+    delay(dt);
   }
 }
