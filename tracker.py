b6d421c4df0a4201adcb11e1ba01d682c27a4776
Juan <jsalazar04@hotmail.com>
2020-09-05 14:22:49 -0400

    changed serial port to /dev/serial0, updated TLE, modified serial write code

--- tracker.py
+++ tracker.py
@@ -1,3 +1,4 @@
+
 # Juan Salazar 5/31/2018
 # Uses PyEphem Package to compute the position of the ISS 
 # and to write it in terms of altitude (degrees above horizon, +N) and azimuth (degrees along horizon, +E) 
@@ -10,7 +11,7 @@ import math
 import time
 from datetime import datetime
 	
-ser = serial.Serial('/dev/ttyAMA0',9600) #initializes connection with the arduino
+ser = serial.Serial('/dev/serial0',9600) #initializes connection with the arduino
 #time.sleep(5.0)
 
 
@@ -24,20 +25,22 @@ home.elevation = 6
 # NORAD TLE (Two Line Element) taken from https://www.celestrak.com/NORAD/elements/stations.txt, website updates several times daily
 # TLE's were invented to package all of the important information about the Keplerian orbits of artificial satellites, 
 # initially designed for 80-hole punch cards
-iss = ephem.readtle("ISS (ZARYA)", 
-	"1 25544U 98067A   19168.89165794  .00000725  00000-0  19994-4 0  9994",
-	"2 25544  51.6438 357.8011 0008176  60.1415  17.8944 15.51206633175360"
-)     
-   
+iss = ephem.readtle("ISS (ZARYA)",
+"1 25544U 98067A   20249.67279622  .00000384  00000-0  15023-4 0  9998",
+"2 25544  51.6467 310.1651 0002005  85.3959  52.3313 15.49214300244493"
+)
+
 while True:
 	home.date = datetime.now()
 	iss.compute(home)
 	alt = math.degrees(iss.alt)
 	az = math.degrees(iss.az)
-	string_pos = str(alt) + " " + str(az)
+        ser.flushInput()
+        ser.flushOutput()
+	string_pos = str(alt) + "," + str(az) + "\n"
 	# print("%s %s" % (ephem.degrees(iss.sublong), ephem.degrees(iss.sublat)))
 	ser.write(string_pos)
-	ser.flush()
+	#ser.flush()
 	print('Altitude: %f - Azimuth: %f' % (alt, az))
 	print(home.date)
 	time.sleep(1) 
